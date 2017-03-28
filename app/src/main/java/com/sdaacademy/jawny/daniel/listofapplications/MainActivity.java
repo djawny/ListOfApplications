8f84706dc41dd3215d38b884e6320a2a7e5517d0
djawny <djawny@op.pl>
2017-03-28 18:12:01 +0200

    Initial commit

---
+++ app/src/main/java/com/sdaacademy/jawny/daniel/listofapplications/MainActivity.java
@@ -0,0 +1,33 @@
+package com.sdaacademy.jawny.daniel.listofapplications;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.menu.main_activity_menu:
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
