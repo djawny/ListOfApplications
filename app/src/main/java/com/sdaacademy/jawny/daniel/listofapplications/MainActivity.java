fbdab430c80773e2ae2f02e125f5576fe82c23cb
djawny <djawny@op.pl>
2017-03-28 19:25:52 +0200

    zmiana

--- app/src/main/java/com/sdaacademy/jawny/daniel/listofapplications/MainActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/listofapplications/MainActivity.java
@@ -1,16 +1,41 @@
 package com.sdaacademy.jawny.daniel.listofapplications;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @BindView(R.id.main_activity_layout)
+    CoordinatorLayout mMainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        showSnackBar();
+    }
+
+    private void showSnackBar() {
+        final Snackbar snackbar = Snackbar.make(mMainLayout, "Snackbar", Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction("Ok", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        snackbar.show();
     }
 
     @Override
@@ -22,10 +47,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.menu.main_activity_menu:
-
+            case R.id.main_menu_settings:
+                openSettings();
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openSettings() {
+        SettingsDialogFragment settingsDialogFragment = SettingsDialogFragment.newInstance();
+        settingsDialogFragment.setCancelable(false);
+        settingsDialogFragment.show(getSupportFragmentManager(),"");
     }
 }
---
+++ app/src/main/java/com/sdaacademy/jawny/daniel/listofapplications/SettingsDialogFragment.java
@@ -0,0 +1,71 @@
+package com.sdaacademy.jawny.daniel.listofapplications;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SettingsDialogFragment extends DialogFragment {
+
+    public static final String TAG = SettingsDialogFragment.class.getSimpleName();
+    @BindView(R.id.show_app_check_box)
+    CheckBox mShowAppCheckBox;
+
+    public SettingsDialogFragment() {
+        // Required empty public constructor
+    }
+
+    public static SettingsDialogFragment newInstance() {
+        return new SettingsDialogFragment();
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View view = getActivity().getLayoutInflater().inflate(R.layout.fragment_settings_dialog, null);
+        ButterKnife.bind(this, view);
+        return new AlertDialog.Builder(getActivity())
+                .setView(view)
+                .setTitle("Settings")
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (mShowAppCheckBox.isChecked()) {
+                            Log.d(TAG, "checked");
+
+                        } else {
+                            Log.d(TAG, "unchecked");
+
+                        }
+                    }
+                })
+                .create();
+    }
+
+    //    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+
+
+}
