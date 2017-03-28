9127e08f8e7d5ff1e1aa61f5ef2005edddf3e599
djawny <djawny@op.pl>
2017-03-28 21:01:04 +0200

    zmiana

--- app/src/main/java/com/sdaacademy/jawny/daniel/listofapplications/AppDetailsDialogFragment.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/listofapplications/AppDetailsDialogFragment.java
@@ -6,15 +6,23 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.util.Log;
 import android.view.View;
-import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AppDetailsDialogFragment extends DialogFragment {
 
+    @BindView(R.id.app_uid)
+    TextView mUid;
+
+    @BindView(R.id.app_name)
+    TextView mName;
+
+    @BindView(R.id.app_icon)
+    ImageView mIcon;
 
     public AppDetailsDialogFragment() {
         // Required empty public constructor
@@ -27,12 +35,14 @@ public class AppDetailsDialogFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        View view = getActivity().getLayoutInflater().inflate(R.layout.fragment_settings_dialog, null);
+        View view = getActivity().getLayoutInflater().inflate(R.layout.fragment_app_details_dialog, null);
         ButterKnife.bind(this, view);
+
+
         return new AlertDialog.Builder(getActivity())
                 .setView(view)
                 .setTitle("App details")
-                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
